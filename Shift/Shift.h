7d878d8fba71d5f36746a2b2ed94895d7011dbb9
mattthousand <matt.buckley@raizlabs.com>
2015-12-10 11:05:10 -0500

    Create Shift framework.

---
+++ Shift/Shift.h
@@ -0,0 +1,19 @@
+//
+//  Shift.h
+//  Shift
+//
+//  Created by Matthew Buckley on 12/10/15.
+//  Copyright © 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shift.
+FOUNDATION_EXPORT double ShiftVersionNumber;
+
+//! Project version string for Shift.
+FOUNDATION_EXPORT const unsigned char ShiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shift/PublicHeader.h>
+
+
